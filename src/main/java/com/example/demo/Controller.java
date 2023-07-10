bb3f113c6a5e66acffe9418df00591d5da0d8fc1
zurieldelacruz <128268732+zurieldelacruz@users.noreply.github.com>
2023-07-10 01:26:18 +0000

    deploy en render

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import java.lang.String;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping(path="/")
+public class Controller{
+
+    @GetMapping(path="/")
+    public String home(){
+        return "Hola desde CodeSpaces";
+    }
+
+}
